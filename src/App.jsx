cfa22eb61fc873e7b24d0857f8d37fedfdae9341
daniel-dieguez <danguez2001@gmail.com^C>
2024-06-05 15:19:49 -0600

    Solo se tiene el componente calculadora en App jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,19 @@
-import React from 'react'
+import React, { useReducer, useState } from 'react'
 import Calculadora from './assets/Calculadora'
-import Ejempl from './assets/Ejempl'
 import style from '../src/style/style.module.css'
 
 
 
-
+// 
 export default function App() {
+
+
+
+
   return (
-    <div className={style.Calculadora}>
-    <h1 className={style.titulo}>Resultado</h1>
-    <div className={style.output}>
-      <div className={style.previos}></div>
-      <div className={style.currento}> </div>
-    </div>
-
-    <input type="text" />
-    <button>DEL</button>
-    <button className={style.ac}>AC</button>
-    <button value="/">/</button>
-    <button >1</button>
-    <button value="2" >2</button>
-    <button value="3">3</button>
-    <button value="4">4</button>
-    <button value="5">5</button>
-    <button value="6">6</button>
-    <button value="7">7</button>
-    <button value="8">8</button>
-    <button value="9">9</button>
-    <button value="+">+</button>
-    <button value="-">-</button>
-    <button value=".">.</button>
-    <button value="0">0</button>
-    <button value="9">=</button>
-
-  </div>
+   <div>
+    <Calculadora>
+    </Calculadora>
+   </div>
   )
 }
